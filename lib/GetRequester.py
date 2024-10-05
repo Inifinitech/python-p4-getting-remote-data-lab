0e5f1c6e0cb08494e1a839e7eebb82ba4b93fd75
Kush <kuriaisac@gmail.com>
2024-10-05 12:30:49 +0300

    updates changes

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,25 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            
+            return response.content
+        except requests.exceptions.RequestException as e:
+            print(f'Error {e}')
+            return b""
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        if response_body:
+            try:
+                return json.loads(response_body)
+            except ValueError as e:
+                print(f"Error decoding {e}")
+        return None
+    
+if __name__ == "__main__":
+    requester = GetRequester("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+    data = requester.load_json()
+    print("Final Data:", data) 
